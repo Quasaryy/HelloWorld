890f0dd60267fd03cd37b150ee35d7e269f8bed6
Yury <quasaryy@gmail.com>
2022-12-27 21:47:08 +0400

    Added button logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
     }
-
+        
+    @IBAction func buttonAction() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 
 }
 
